6ff722d34f3a4dcdbdd4db00c2925958df4ae869
Parsa Zoghi <106579602+ParsaZoghi@users.noreply.github.com>
2022-08-07 16:27:02 +0430

    Update master.js

--- assets/master.js
+++ assets/master.js
@@ -1,6 +1,8 @@
 document.querySelector('title').innerHTML = 'BMI Calculator'
 
 document.head.appendChild(document.createElement('style')).innerHTML = `
+    @import url("https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css");
+
     :root {
         --color1: #4cecc4;
         --color2: #91efda;
@@ -8,6 +10,7 @@ document.head.appendChild(document.createElement('style')).innerHTML = `
         --color4: #7c8484;
         --color5: #b2ccc4;
         --color6: #444444;
+        --color7: #f0d240;
 
         background: var(--color3);
     }
@@ -19,15 +22,27 @@ document.head.appendChild(document.createElement('style')).innerHTML = `
         font-family: Arial, "Helvetica Neue", Helvetica, sans-serif;
     }
 
+    a {
+        text-decoration: none;
+        color: inherit;
+    }
+
+    li {
+        list-style-type: none;
+    }
+
+    
     main {
         width: 100%;
         height: 100vh;
-        padding: 50px;
+        padding: 30px;
         display: flex;
-        justify-content: center;
+        flex-direction: column;
+        justify-content: space-evenly;
         align-items: center;
     }
-    main > section {
+
+    main > section:nth-of-type(1) {
         width: 400px;
         height: 450px;
         display: flex;
@@ -39,7 +54,8 @@ document.head.appendChild(document.createElement('style')).innerHTML = `
         box-shadow:  35px 35px 70px #0a654f,
                     -35px -35px 70px #0e896b;
     }
-    main > section > h1 {
+
+    main > section:nth-of-type(1) > h1 {
         display: flex;
         justify-content: center;
         align-items: center;
@@ -47,19 +63,15 @@ document.head.appendChild(document.createElement('style')).innerHTML = `
         font-size: 1.6rem;
     }
 
-    main > section > div {
+    main > section:nth-of-type(1) > div {
         border-radius: inherit;
         display: flex;
         flex-direction: column;
         justify-content: space-evenly;
         align-items: center;
     }
-    main > section > div:nth-of-type(1) {
-        height: 180px;
-        margin: 40px;
-    }
     
-    main > section > div button {
+    main > section:nth-of-type(1) > div button {
         padding: 15px 45px;
         color: var(--color5);
         font-size: 1rem;
@@ -74,7 +86,7 @@ document.head.appendChild(document.createElement('style')).innerHTML = `
         cursor: pointer;
         border: 2px solid var(--color5);
     }
-    main > section > div button:hover {
+    main > section:nth-of-type(1) > div button:hover {
         transform: scale(1.1);
         color: var(--color3);
         background-color: var(--color5);
@@ -83,11 +95,84 @@ document.head.appendChild(document.createElement('style')).innerHTML = `
                     inset 0 0 0 0 var(--color5);
         text-shadow: 0 0 0 var(--color5);
     }
-    main > section > div button:active {
+    main > section:nth-of-type(1) > div button:active {
         box-shadow: 0 0 0 0 var(--color5),
                     0 0 0 0 var(--color1),
                     inset 0 0 0 0 var(--color5);
         text-shadow: 0 0 0 var(--color5);
+    }
+
+    main > section:nth-of-type(1) > div:nth-of-type(1) {
+        height: 180px;
+        margin: 40px;
+    }
+
+    main > section:nth-of-type(2) {
+        width: 130px;
+        height: 30px;
+        display: flex;
+        position: relative;
+        color: var(--color2);
+        overflow: hidden;
+    }
+
+    main > section:nth-of-type(2) > div {
+        width: 125%;
+        height: inherit;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        position: absolute;
+        right: 79%; top: 50%;
+        transform: translate(0, -50%);
+        transition: .5s;
+    }
+    main > section:nth-of-type(2) >div:hover{
+        right: -25%;
+        transform: translate(0, -50%);
+    }
+
+    main > section:nth-of-type(2) > div:hover > a:first-child {
+        margin-left: 5.5px;
+    }
+
+    main > section:nth-of-type(2) >div:hover + a.website {
+        transform: translate(200%, -50%);
+    }
+
+    main > section:nth-of-type(2) > div > a {
+        display: grid;
+        place-items: center;
+        padding: 3px;
+        transition: inherit;
+    }
+
+    main > section:nth-of-type(2) > div > a > i {
+        font-size: 1.1rem;
+        display: grid;
+        place-items: center;
+    }
+
+    main > section:nth-of-type(2) > div > div {
+        font-size: 1.1rem;
+        transition: inherit;
+    }
+
+    main > section:nth-of-type(2) > a.website {
+        width: fit-content;
+        padding: 3px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        position: absolute;
+        left: 50%; top: 50%;
+        transform: translate(-36%, -50%);
+        transition: .5s;
+    }
+
+    main > section:nth-of-type(2) a:hover {
+        color: var(--color7);
+        transition: color .3s;
     }
 `
 
@@ -98,9 +183,9 @@ let _selectSection = document.getElementsByTagName('section')
 _selectSection[0].appendChild(document.createElement('h1')).innerHTML = 'BMI Calculator'
 
 _selectSection[0].appendChild(document.createElement('div'))
-let _selectDiv1 = document.getElementsByTagName('div')
-_selectDiv1[0].appendChild(document.createElement('button')).innerHTML = 'Enter your <strong>WEIGHT</strong> (kg)'
-_selectDiv1[0].appendChild(document.createElement('button')).innerHTML = 'Enter your <strong>HEIGHT</strong> (cm)'
+let _selectDiv = document.getElementsByTagName('div')
+_selectDiv[0].appendChild(document.createElement('button')).innerHTML = 'Enter your <strong>WEIGHT</strong> (kg)'
+_selectDiv[0].appendChild(document.createElement('button')).innerHTML = 'Enter your <strong>HEIGHT</strong> (cm)'
 
 let _weightArgu = ''
 function _weightFunc() {
@@ -165,3 +250,18 @@ function _bmiFunc() {
 }
 document.getElementsByTagName('button')[2].setAttribute('onclick', '_bmiFunc()')
 
+document.getElementsByTagName('main')[0].appendChild(document.createElement('section'))
+
+_selectSection[1].appendChild(document.createElement('div'))
+_selectDiv[2].appendChild(document.createElement('a')).appendChild(document.createElement('i')).classList.add('bi', 'bi-instagram')
+let _selectA = document.getElementsByTagName('a')
+_selectA[0].href = "https://www.instagram.com/parsa_zoghi_web/"
+_selectDiv[2].appendChild(document.createElement('a')).appendChild(document.createElement('i')).classList.add('bi', 'bi-github')
+_selectA[1].href = "https://github.com/parsazoghi"
+_selectDiv[2].appendChild(document.createElement('a')).appendChild(document.createElement('i')).classList.add('bi', 'bi-linkedin')
+_selectA[2].href = "https://www.linkedin.com/in/parsa-zoghi-445ab9210"
+_selectDiv[2].appendChild(document.createElement('div')).appendChild(document.createElement('i')).classList.add('bi', 'bi-arrow-right')
+
+_selectSection[1].appendChild(document.createElement('a')).href = "https://parsazoghi.com"
+_selectA[3].innerHTML = 'Parsa Zoghi'
+_selectA[3].classList.add('website')
